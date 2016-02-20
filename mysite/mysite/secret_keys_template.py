353965b3dcaf4b2165e5c910acbe3caf2d764dcc
Adrien <admanaus@gmail.com>
2016-02-19 22:13:51 -0700

    Created a secret_keys_template file
    This file acts as a place holder for the keys of anyone branching this
    project.

---
+++ mysite/mysite/secret_keys_template.py
@@ -0,0 +1,10 @@
+# You will want to store your secret keys and 
+# passwords in a seperate file from settings.py.
+
+# That way you can have GitHub ignore your secret_keys.py 
+# file and still pblish the rest to a public repository.
+
+# Simply import secret_keys.py into settings.py 
+
+SECRET_KEY = 'blah blah'
+
